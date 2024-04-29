5f0ba5d80f7170df1e27a3f9ba2a8ce5148d0f9f
Ahmed Dalton <108447999+Ahmed-Eldalatony@users.noreply.github.com>
2024-04-29 10:20:12 +0300

    Delete apps/api directory

--- apps/api/src/dtos/UserDto.ts
+++
@@ -1,8 +0,0 @@
-export interface UserDto {
-  _id: string;
-  username: string;
-  email: string;
-  password: string;
-  image: string;
-  tasks: string[];
-}
--- apps/api/src/routes/auth.route.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller";
-
-export const authRouter = express.Router();
-//@ts-ignore
-authRouter.post("/signup", signup);
-authRouter.post("/login", login);
-authRouter.get("/logout", logout);
