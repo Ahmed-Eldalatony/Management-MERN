7a7ed0d486610ee389028d98cb8f845c85221784
Ahmed-Eldalatony <a01141399246@gmail.com>
2024-05-01 14:33:22 -0300

    serverless functions

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -9,14 +9,18 @@ import taskRouter from "./routes/task.route";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { protectRoutes } from "./middleware/requireAuth";
+import serverless from "serverless-http";
+import { Router } from "express";
 
 require("dotenv").config();
 
 const port = 3000;
 export const app = express();
+const router = Router();
+
 mongoConnect();
 
-app.use(
+router.use(
   express.urlencoded({
     extended: true,
     limit: 10000,
@@ -24,20 +28,23 @@ app.use(
   })
 );
 
-app.use(cookieParser());
-app.use(cors());
-app.use(express.json());
-app.use(express.static("uploads"));
-app.use("/api/auth", upload, authRouter);
-app.use("/api/user", upload, userRouter);
-app.use("/api/", protectRoutes, taskRouter);
+router.use(cookieParser());
+router.use(cors());
+router.use(express.json());
+router.use(express.static("uploads"));
+router.use("/api/auth", upload, authRouter);
+router.use("/api/user", upload, userRouter);
+router.use("/api/", protectRoutes, taskRouter);
 
-app.use("/api", (req: Request, res: Response) => {
+router.use("/api", (req: Request, res: Response) => {
   res.json("hello  from the route");
 });
+app.use("/api", router);
 
 app.listen(port, () => {
   console.log("app is listening on port" + port);
 });
 
 app.use(errorMiddleware);
+
+export const handler = serverless(app);
